94d6f2b2b6365149659d541d2f7294a98e9b88d1
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-12 11:46:47 +0300

    improve activity

--- app/src/main/java/com/pakollya/paginglist/MainActivity.kt
+++ app/src/main/java/com/pakollya/paginglist/MainActivity.kt
@@ -2,10 +2,25 @@ package com.pakollya.paginglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.pakollya.paginglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel = MessagesViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        val adapter = Adapter(viewModel)
+
+        binding.recyclerView.adapter = adapter
+
+        viewModel.init(savedInstanceState == null)
+
+        viewModel.observe(this) {
+            adapter.update(it)
+        }
     }
 }
