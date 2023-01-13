15bd9f8620ef58d7244cb319682824ce9fe2c940
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-13 11:47:28 +0300

    improve ui

--- app/src/main/java/com/pakollya/paginglist/BaseView.kt
+++ app/src/main/java/com/pakollya/paginglist/BaseView.kt
@@ -5,4 +5,5 @@ import android.view.View
 interface BaseView {
     fun show(text: CharSequence) = Unit
     fun handleClick(listener: View.OnClickListener) = Unit
+    fun select(selected: Boolean) = Unit
 }
--- app/src/main/java/com/pakollya/paginglist/CustomView.kt
+++ app/src/main/java/com/pakollya/paginglist/CustomView.kt
@@ -2,6 +2,8 @@ package com.pakollya.paginglist
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 
 class CustomTextView : androidx.appcompat.widget.AppCompatTextView, BaseView {
     constructor(context: Context) : super(context)
@@ -14,5 +16,24 @@ class CustomTextView : androidx.appcompat.widget.AppCompatTextView, BaseView {
 
     override fun show(text: CharSequence) {
         setText(text)
+    }
+}
+
+class CustomConstraintLayout : ConstraintLayout, BaseView {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    override fun select(selected: Boolean) {
+        setBackgroundColor(
+            ContextCompat.getColor(
+                this.context,
+                if (selected) R.color.selected else R.color.white
+            )
+        )
     }
 }
