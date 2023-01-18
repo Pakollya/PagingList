5983e6518d6bddc1d940e11c859ccdf48668cd1a
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-18 09:43:00 +0300

    add dao

---
+++ app/src/main/java/com/pakollya/paginglist/MessagesDao.kt
@@ -0,0 +1,22 @@
+package com.pakollya.paginglist
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface MessagesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMessages(messages: List<Message.Data>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addMessage(messages: Message.Data)
+
+    @Query("SELECT timestamp FROM messages WHERE id LIKE :id")
+    fun dateById(id: Long): Long
+
+    @Query("SELECT * FROM messages ORDER BY timestamp")
+    fun messages(): List<Message.Data>
+}
