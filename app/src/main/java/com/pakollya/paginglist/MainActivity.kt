8a31d6bef203514d5b50369da9a6a339af66805b
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-18 11:12:36 +0300

    refactor activity

--- app/src/main/java/com/pakollya/paginglist/MainActivity.kt
+++ app/src/main/java/com/pakollya/paginglist/MainActivity.kt
@@ -1,29 +1,29 @@
 package com.pakollya.paginglist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.pakollya.paginglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel = MessagesViewModel()
+    private lateinit var viewModel: MessagesViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
+        viewModel = DependencyContainer.Base.provideViewModel(this.applicationContext)
+
         val controller = MessageController(
             viewModel,
             object : ClickListener {
-                override fun click(id: Long) {
-                    val randomId = viewModel.randomId()
+                override fun click() {
+                    val randomId = 120
                     viewModel.mapId(randomId)
                     viewModel.loadPageById(randomId)
-
-                    val position = viewModel.positionById(randomId)
-                    binding.recyclerView.scrollToPosition(position)
                 }
             }
         )
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.addMessageButton.setOnClickListener{
             viewModel.addMessage()
+            //TODO: check scroll
             binding.recyclerView.scrollToPosition(controller.adapter.itemCount - 1)
         }
 
@@ -43,6 +44,11 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.observeId(this) {
             Toast.makeText(this, "Move to $it item",Toast.LENGTH_LONG).show()
+        }
+
+        viewModel.observePosition(this) {
+            binding.recyclerView.scrollToPosition(it)
+            Log.e("Activity position", "$it")
         }
     }
 }
