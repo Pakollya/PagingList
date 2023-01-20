6cb55795dfaeafe8f433a7a41041159693f1c7e5
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-20 10:07:16 +0300

    add dayPart

---
+++ app/src/main/java/com/pakollya/paginglist/DayPart.kt
@@ -0,0 +1,17 @@
+package com.pakollya.paginglist
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "parts")
+data class DayPart(
+    @PrimaryKey(autoGenerate = true)
+    val index: Int = 0,
+    val pageIndex: Int,
+    val startId: Long,
+    val endId: Long,
+    val reminder: Boolean,
+    val startPosition: Int,
+    val endPosition: Int,
+    val date: Long,
+)
