04523db0f2be043de706569a2549bc6f620f3b13
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-20 10:09:25 +0300

    add migrations database

--- app/src/main/java/com/pakollya/paginglist/MessagesCache.kt
+++ app/src/main/java/com/pakollya/paginglist/MessagesCache.kt
@@ -2,6 +2,8 @@ package com.pakollya.paginglist
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 interface MessagesCache {
     fun dataBase(): MessagesDatabase
@@ -12,9 +14,27 @@ interface MessagesCache {
                 context.applicationContext,
                 MessagesDatabase::class.java,
                 "messages"
-            ).build()
+            )
+                .addMigrations(MIGRATION_1_2)
+                .addMigrations(MIGRATION_2_3)
+                .build()
         }
 
         override fun dataBase() = database
+
+        companion object{
+            private val MIGRATION_1_2 = object : Migration(1, 2) {
+                override fun migrate(database: SupportSQLiteDatabase) {
+                    database.execSQL("CREATE TABLE pages ('index' INTEGER PRIMARY KEY NOT NULL, startId INTEGER NOT NULL DEFAULT 0, endId INTEGER NOT NULL DEFAULT 0)")
+                    database.execSQL("CREATE TABLE parts ('index' INTEGER PRIMARY KEY NOT NULL, pageIndex INTEGER NOT NULL DEFAULT 0, startId INTEGER NOT NULL DEFAULT 0, endId INTEGER NOT NULL DEFAULT 0, reminder INTEGER NOT NULL DEFAULT 0, startPosition INTEGER NOT NULL DEFAULT 0, endPosition INTEGER NOT NULL DEFAULT 0, date INTEGER NOT NULL DEFAULT 0)")
+                }
+            }
+
+            private val MIGRATION_2_3 = object : Migration(2, 3) {
+                override fun migrate(database: SupportSQLiteDatabase) {
+                    database.execSQL("DROP TABLE days")
+                }
+            }
+        }
     }
 }
