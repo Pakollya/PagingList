16932c2879f9a86c4bbf3ed15d8dccef4437412a
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-20 10:07:48 +0300

    add pages dao

---
+++ app/src/main/java/com/pakollya/paginglist/PagesDao.kt
@@ -0,0 +1,18 @@
+package com.pakollya.paginglist
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface PagesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPages(pages: List<Page>)
+
+    @Query("SELECT `index` FROM pages WHERE startId <= :id AND endId >= :id")
+    fun pageIndexById(id: Long): Int
+
+    @Query("DELETE FROM pages")
+    fun deletePages()
+}
