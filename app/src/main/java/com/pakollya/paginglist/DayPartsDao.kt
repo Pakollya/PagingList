dfd2ef34a3d63a631d81e495439cc6208bd47f0a
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-20 10:07:28 +0300

    add dayParts dao

---
+++ app/src/main/java/com/pakollya/paginglist/DayPartsDao.kt
@@ -0,0 +1,30 @@
+package com.pakollya.paginglist
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface DayPartsDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertDayParts(days: List<DayPart>)
+
+    @Query("SELECT * FROM parts ORDER BY date")
+    fun dayParts(): List<DayPart>
+
+    @Query("SELECT * FROM parts WHERE pageIndex LIKE :index ORDER BY date")
+    fun dayPartsByPage(index: Int): List<DayPart>
+
+    @Query("SELECT * FROM parts WHERE startId <= :id AND endId >= :id")
+    fun dayPartById(id: Long): DayPart
+
+    @Query("SELECT endPosition FROM parts WHERE endId LIKE :lastId")
+    fun lastPositionById(lastId: Long): Int
+
+    @Query("DELETE FROM parts WHERE pageIndex = :index")
+    fun deletePartsByPage(index: Int)
+
+    @Query("DELETE FROM parts")
+    fun deleteDayParts()
+}
