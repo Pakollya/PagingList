ae9c2444f10836a5a6f4ce7ab665e06edbb0c0ab
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-24 10:03:51 +0300

    add loading in Load

--- app/src/main/java/com/pakollya/paginglist/presentation/common/Load.kt
+++ app/src/main/java/com/pakollya/paginglist/presentation/common/Load.kt
@@ -1,10 +1,18 @@
 package com.pakollya.paginglist.presentation.common
 
-interface Load : LoadNext, LoadPrevious
+interface Load : LoadNext, LoadPrevious, MapIsLoading, IsLoading
 interface LoadNext{
     fun loadNext()
 }
 
 interface LoadPrevious{
     fun loadPrevious()
+}
+
+interface MapIsLoading {
+    fun mapIsLoading(isLoading: Boolean)
+}
+
+interface IsLoading {
+    fun isLoading(): Boolean
 }
