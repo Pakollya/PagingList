7ee2480c085c54d0c2cd4d1e3377940c1d876f8d
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-30 12:04:59 +0300

    add pagingdata in Communication

--- app/src/main/java/com/pakollya/paginglist/presentation/Communication.kt
+++ app/src/main/java/com/pakollya/paginglist/presentation/Communication.kt
@@ -3,27 +3,31 @@ package com.pakollya.paginglist.presentation
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.paging.PagingData
+import com.pakollya.paginglist.data.cache.message.Message
 import com.pakollya.paginglist.presentation.common.Observe
 
-interface Communication: Observe, IsSelectedId, IsLoading {
+interface Communication : Observe, IsSelectedId {
 
     fun showNewId(id: Int)
 
     fun showPosition(position: Int)
 
-    fun showMessages(messages: MessagesPageUi)
-
     fun showProgress(show: Int)
 
-    fun mapIsLoading(isLoading: Boolean)
+    fun showData(data: PagingData<Message>)
 
     class Base(
         private val id: MutableLiveData<Int> = MutableLiveData<Int>(),
         private val position: MutableLiveData<Int> = MutableLiveData<Int>(),
-        private val messages: MutableLiveData<MessagesPageUi> = MutableLiveData<MessagesPageUi>(),
         private val progress: MutableLiveData<Int> = MutableLiveData<Int>(),
-        private val isLoading: MutableLiveData<Boolean> = MutableLiveData<Boolean>(false)
-    ): Communication {
+        private val data: MutableLiveData<PagingData<Message>> = MutableLiveData<PagingData<Message>>()
+    ) : Communication {
+        
+        override fun showData(data: PagingData<Message>) {
+            this.data.postValue(data)
+        }
+        
         override fun showNewId(id: Int) {
             this.id.value = id
         }
@@ -32,26 +36,13 @@ interface Communication: Observe, IsSelectedId, IsLoading {
             this.position.postValue(position)
         }
 
-        override fun showMessages(messages: MessagesPageUi) {
-            this.messages.value = messages
-        }
-
         override fun showProgress(show: Int) {
             this.progress.postValue(show)
         }
 
-        override fun mapIsLoading(isLoading: Boolean) {
-            this.isLoading.value = isLoading
-        }
-
-        override fun isLoading(): Boolean {
-            val value = this.isLoading.value
-            return value == true
-        }
-
         override fun isSelectedId(id: Long): Boolean {
             val value = this.id.value
-            return if(value == null)
+            return if (value == null)
                 false
             else
                 value.toLong() == id
@@ -65,20 +56,16 @@ interface Communication: Observe, IsSelectedId, IsLoading {
             position.observe(owner, observer)
         }
 
-        override fun observeMessages(owner: LifecycleOwner, observer: Observer<MessagesPageUi>) {
-            messages.observe(owner, observer)
-        }
-
         override fun observeProgress(owner: LifecycleOwner, observer: Observer<Int>) {
             progress.observe(owner, observer)
+        }
+
+        override fun observeData(owner: LifecycleOwner, observer: Observer<PagingData<Message>>) {
+            data.observe(owner, observer)
         }
     }
 }
 
 interface IsSelectedId {
     fun isSelectedId(id: Long): Boolean
-}
-
-interface IsLoading {
-    fun isLoading(): Boolean
 }
--- app/src/main/java/com/pakollya/paginglist/presentation/common/Observe.kt
+++ app/src/main/java/com/pakollya/paginglist/presentation/common/Observe.kt
@@ -2,6 +2,8 @@ package com.pakollya.paginglist.presentation.common
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
+import androidx.paging.PagingData
+import com.pakollya.paginglist.data.cache.message.Message
 import com.pakollya.paginglist.presentation.MessagesPageUi
 
 interface Observe {
@@ -9,7 +11,7 @@ interface Observe {
 
     fun observePosition(owner: LifecycleOwner, observer: Observer<Int>)
 
-    fun observeMessages(owner: LifecycleOwner, observer: Observer<MessagesPageUi>)
-
     fun observeProgress(owner: LifecycleOwner, observer: Observer<Int>)
+    
+    fun observeData(owner: LifecycleOwner, observer: Observer<PagingData<Message>>)
 }
