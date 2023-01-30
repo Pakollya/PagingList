b0ba7083d13d4d540ff70fc5a9fe4f2a7e18d14b
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-30 11:59:32 +0300

    add MessageController as PagingDataEpoxyController

--- app/src/main/java/com/pakollya/paginglist/presentation/epoxy/MessageController.kt
+++ app/src/main/java/com/pakollya/paginglist/presentation/epoxy/MessageController.kt
@@ -1,74 +1,30 @@
 package com.pakollya.paginglist.presentation.epoxy
 
-import com.airbnb.epoxy.EpoxyController
+import com.airbnb.epoxy.EpoxyModel
+import com.airbnb.epoxy.paging3.PagingDataEpoxyController
 import com.pakollya.paginglist.data.cache.message.Message
-import com.pakollya.paginglist.data.MessagesRepository.Strategy.*
-import com.pakollya.paginglist.presentation.MessagesPageUi
 import com.pakollya.paginglist.presentation.common.ClickListener
-import com.pakollya.paginglist.presentation.common.Load
 
 class MessageController(
-    private val load: Load,
     private val clickListener: ClickListener
-) : EpoxyController() {
-    private var listPageUi = mutableListOf<MessagesPageUi>()
-    private var list = mutableListOf<Message>()
+) : PagingDataEpoxyController<Message>() {
 
-    fun update(ui: MessagesPageUi) {
-        when (ui.strategy) {
-            INIT -> init(ui)
-            NEXT -> next(ui)
-            PREVIOUS -> previous(ui)
-        }
-
-        requestModelBuild()
-    }
-
-    fun init(ui: MessagesPageUi) {
-        listPageUi.clear()
-        list.clear()
-
-        listPageUi.add(ui)
-        list.addAll(ui.messages)
-        load.mapIsLoading(false)
-    }
-
-    fun next(ui: MessagesPageUi) {
-        if (listPageUi.size >= 2) {
-            list.subList(0, listPageUi[0].pageSize).clear()
-            listPageUi.removeAt(0)
-        }
-        listPageUi.add(ui)
-        list.addAll(ui.messages)
-        load.mapIsLoading(false)
-    }
-
-    fun previous(ui: MessagesPageUi) {
-        if (listPageUi.size >= 2) {
-            list.subList(listPageUi[0].pageSize, listPageUi[0].pageSize + listPageUi[1].pageSize).clear()
-            listPageUi.removeLast()
-        }
-        listPageUi.add(0, ui)
-        list.addAll(0, ui.messages)
-        load.mapIsLoading(false)
-    }
-
-    override fun buildModels() {
-        list.forEach { item ->
-            when (item) {
-                is Message.Header -> {
-                    header {
-                        id(item.index())
-                        date(item.date)
-                    }
-                }
-                is Message.Data -> {
-                    message {
-                        id(item.index())
-                        message(item)
-                        click(this@MessageController.clickListener)
-                    }
-                }
+    override fun buildItemModel(currentPosition: Int, item: Message?): EpoxyModel<*> {
+        return when (item) {
+            null -> {
+                PlaceHolderModel_()
+                    .id(1L)
+            }
+            is Message.Header -> {
+                HeaderModel_()
+                    .id(item.index())
+                    .date(item.date)
+            }
+            else -> {
+                MessageModel_()
+                    .id(item.index())
+                    .message(item)
+                    .click(clickListener)
             }
         }
     }
