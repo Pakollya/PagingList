97dc34f43d3dc8ec4e813d2d9a2b9552cc6ea9ed
nick <n1ck.bl4ck0ut@gmail.com>
2023-01-31 11:41:03 +0300

    remove unwanted from DayPartsDao

--- app/src/main/java/com/pakollya/paginglist/data/cache/day/DayPartsDao.kt
+++ app/src/main/java/com/pakollya/paginglist/data/cache/day/DayPartsDao.kt
@@ -10,17 +10,8 @@ interface DayPartsDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertDayParts(days: List<DayPart>)
 
-    @Query("SELECT * FROM parts ORDER BY date")
-    fun dayParts(): List<DayPart>
-
-    @Query("SELECT * FROM parts WHERE pageIndex LIKE :index ORDER BY date")
-    fun dayPartsByPage(index: Int): List<DayPart>
-
     @Query("SELECT * FROM parts WHERE startId <= :id AND endId >= :id")
     fun dayPartById(id: Long): DayPart
-
-    @Query("SELECT endPosition FROM parts WHERE endId LIKE :lastId")
-    fun lastPositionById(lastId: Long): Int
 
     @Query("DELETE FROM parts WHERE pageIndex = :index")
     fun deletePartsByPage(index: Int)
